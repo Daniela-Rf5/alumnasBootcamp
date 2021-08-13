15de9fac3b0a85a52315688f0379edf1baae96ed
Daniela-Rf5 <danielarodriguezlainez@gmail.com>
2021-08-13 13:54:00 +0200

    first commit

---
+++ src/main/java/org/factoriaf5/alumnas/AlumnasApplication.java
@@ -0,0 +1,13 @@
+package org.factoriaf5.alumnas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlumnasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlumnasApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/factoriaf5/alumnas/AlumnasApplicationTests.java
@@ -0,0 +1,13 @@
+package org.factoriaf5.alumnas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlumnasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
