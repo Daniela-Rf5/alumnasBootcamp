1d1214ad996f211142f67e5ef99875b0a1e18467
Daniela-Rf5 <danielarodriguezlainez@gmail.com>
2021-08-15 16:21:53 +0200

    elimando alumnas

--- src/main/java/org/factoriaf5/alumnas/controllers/AlumnasController.java
+++ src/main/java/org/factoriaf5/alumnas/controllers/AlumnasController.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
@@ -30,15 +31,31 @@ public class  AlumnasController {
         model.addAttribute("alumnas", alumnas);
         return "alumnas/all";
     }
-    @GetMapping("/alumnas/new")
-    public String getForm(Model model){
+    @GetMapping("/new")
+    public String newAlumna(Model model){
         Alumna alumna = new Alumna();
         model.addAttribute("alumna", alumna);
+        model.addAttribute("nombre", "Create new alumna");
         return "alumnas/new";
     }
+    @GetMapping("alumnas/edit/{id}")
+    public String editAlumna(Model model, @PathVariable Long id){
+        Alumna alumna = alumnaService.findById(id);
+        model.addAttribute("alumna", alumna);
+        model.addAttribute("nombre", "editar alumna");
+        return "alumnas/edit";
+    }
+
+
     @PostMapping("/alumnas/new")
     String addAlumna(@ModelAttribute Alumna alumna){
         alumnaService.save(alumna);
         return "redirect:/alumnas";
     }
-}
+    @GetMapping("alumnas/eliminar/{id}")
+    public String eliminarAlumna(Model model, @PathVariable Long id){
+        Alumna alumna = alumnaService.findById(id);
+        model.addAttribute("alumna", alumna);
+        model.addAttribute("nombre", "eliminar alumna");
+        return "redirect:/alumnas";
+}}
